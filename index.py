b3d76ae666ea7b847aeed0f415e6dd641f82ff64
hibamallat <hibamallat040@gmail.com>
2024-11-12 03:42:40 +0200

    Complete and test initial code version

--- index.py
+++ index.py
@@ -2,3 +2,18 @@
 Write a function first_missing_positive(numbers) that finds the smallest 
 positive integer that is missing from a given list
 """
+def first_missing_positive(numbers):
+    first_positive = 1
+    for i in range (len(numbers)):
+        if numbers[i] == first_positive:
+            first_positive += 1
+       
+    return first_positive
+
+#array = [3,4,-1,1]
+#array = [1,2,3]
+array = [1, 2, 0]
+print(array)
+print(first_missing_positive(array))
+            
+        
