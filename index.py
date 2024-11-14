211e7810fcd76c418a89a61b772d4cf87f7455d0
hibamallat <hibamallat040@gmail.com>
2024-11-14 03:38:30 +0200

    Get the list input from the user

--- index.py
+++ index.py
@@ -3,7 +3,9 @@ Write a function first_missing_positive(numbers) that finds the smallest
 positive integer that is missing from a given list
 """
 def first_missing_positive(numbers):
+
     first_positive = 1
+
     for i in range (len(numbers)):
         if numbers[i] == first_positive:
             first_positive += 1
@@ -12,8 +14,15 @@ def first_missing_positive(numbers):
 
 #array = [3,4,-1,1]
 #array = [1,2,3]
-array = [1, 2, 0]
-print(array)
-print(first_missing_positive(array))
+#array = [1, 2, 0]
+numbers_list = []
+size = int(input(""))
+
+for i in range (size):
+    num = int(input(f"Enter nb{i+1}: "))
+    numbers_list.append(num)
+
+print(numbers_list)
+print(first_missing_positive(numbers_list))
             
         
